3a97857e85ff252b7f29adc5c5a118ff3ac58e92
Kerem Kaynar <kaynar@dai-lab.de>
2017-07-25 12:03:08 +0200

    First operator screen with navbar is ready.

--- com.waps.core/src/main/java/com/waps/core/controller/WapsController.java
+++ com.waps.core/src/main/java/com/waps/core/controller/WapsController.java
@@ -3,15 +3,30 @@ package com.waps.core.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.waps.core.data.ScanInfo;
 
 @Controller
 public class WapsController {
 
+	private static final String ScanInfo = "scanInfo";
+
 	@RequestMapping("/waps")
 	public String defaultPage(Model model) {
-		model.addAttribute("scanInfo", new ScanInfo());
+		model.addAttribute(ScanInfo, new ScanInfo());
 		return "scan";
+	}
+
+	@RequestMapping(value = "/operators", method = RequestMethod.POST)
+	public String operators(ScanInfo scanInfo, RedirectAttributes redirectAttributes) {
+		redirectAttributes.addFlashAttribute(ScanInfo, scanInfo);
+		return "redirect:/operators/requestParameters";
+	}
+
+	@RequestMapping(value = "/operators/requestParameters")
+	public String requestParameters() {
+		return "requestParameters";
 	}
 }
