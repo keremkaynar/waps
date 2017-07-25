9046d9d72e933935f08f14a7a731453a80fabf8f
Kerem Kaynar <kaynar@dai-lab.de>
2017-07-25 14:51:18 +0200

    reading session attributes from thymeleaf templates

--- com.waps.core/src/main/java/com/waps/core/controller/WapsController.java
+++ com.waps.core/src/main/java/com/waps/core/controller/WapsController.java
@@ -1,27 +1,31 @@
 package com.waps.core.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.waps.core.data.ScanInfo;
 
 @Controller
+@SessionAttributes("scanInfo")
 public class WapsController {
 
 	private static final String ScanInfo = "scanInfo";
 
+	@ModelAttribute(ScanInfo)
+	public ScanInfo getScanInfo() {
+		return new ScanInfo();
+	}
+
 	@RequestMapping("/waps")
-	public String defaultPage(Model model) {
-		model.addAttribute(ScanInfo, new ScanInfo());
+	public String defaultPage() {
 		return "scan";
 	}
 
 	@RequestMapping(value = "/operators", method = RequestMethod.POST)
-	public String operators(ScanInfo scanInfo, RedirectAttributes redirectAttributes) {
-		redirectAttributes.addFlashAttribute(ScanInfo, scanInfo);
+	public String operators(@ModelAttribute(ScanInfo) ScanInfo scanInfo) {
 		return "redirect:/operators/requestParameters";
 	}
 
